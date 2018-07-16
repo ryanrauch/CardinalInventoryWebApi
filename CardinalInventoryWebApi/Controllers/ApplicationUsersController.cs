1d66b7a6c84844a0d643f376821a2388acb35154
Ryan Rauch <rauch.ryan@gmail.com>
2018-07-16 02:25:11 -0500

    applicationusers

---
+++ CardinalInventoryWebApi/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CardinalInventoryWebApi.Data;
+using CardinalInventoryWebApi.Data.Models;
+
+namespace CardinalInventoryWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApplicationUsers
+        [HttpGet]
+        public async Task<IEnumerable<ApplicationUserContract>> GetApplicationUsersAsync()
+        {
+            return await _context.ApplicationUsers.Select(au => new ApplicationUserContract()
+            {
+                Id = au.Id,
+                FirstName = au.FirstName,
+                LastName = au.LastName,
+                Active = au.Active,
+                UserName = au.UserName
+            }).ToListAsync();
+        }
+
+        // GET: api/ApplicationUsers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetApplicationUser([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var applicationUser = await _context.ApplicationUsers.FindAsync(id);
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            var contract = new ApplicationUserContract()
+            {
+                Id = applicationUser.Id,
+                FirstName = applicationUser.FirstName,
+                LastName = applicationUser.LastName,
+                UserName = applicationUser.UserName,
+                Active = applicationUser.Active
+            };
+            return Ok(contract);
+        }
+
+        //// PUT: api/ApplicationUsers/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutApplicationUser([FromRoute] Guid id, [FromBody] ApplicationUser applicationUser)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != applicationUser.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(applicationUser).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!ApplicationUserExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //// POST: api/ApplicationUsers
+        //[HttpPost]
+        //public async Task<IActionResult> PostApplicationUser([FromBody] ApplicationUser applicationUser)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    _context.ApplicationUsers.Add(applicationUser);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetApplicationUser", new { id = applicationUser.Id }, applicationUser);
+        //}
+
+        //// DELETE: api/ApplicationUsers/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteApplicationUser([FromRoute] Guid id)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    var applicationUser = await _context.ApplicationUsers.FindAsync(id);
+        //    if (applicationUser == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.ApplicationUsers.Remove(applicationUser);
+        //    await _context.SaveChangesAsync();
+
+        //    return Ok(applicationUser);
+        //}
+
+        private bool ApplicationUserExists(Guid id)
+        {
+            return _context.ApplicationUsers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CardinalInventoryWebApi/Data/Models/ApplicationUserContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardinalInventoryWebApi.Data.Models
+{
+    public class ApplicationUserContract
+    {
+        public Guid Id { get; set; }
+        public bool Active { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+    }
+}
