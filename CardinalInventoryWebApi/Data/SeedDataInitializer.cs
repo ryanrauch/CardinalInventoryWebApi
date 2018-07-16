eeb9b00be3dd646766e09a7f48dcc675c6afd484
Ryan Rauch <rauch.ryan@gmail.com>
2018-07-16 08:38:30 -0500

    seed

--- CardinalInventoryWebApi/Data/SeedDataInitializer.cs
+++ CardinalInventoryWebApi/Data/SeedDataInitializer.cs
@@ -59,7 +59,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Bulleit Bourbon",
-                        ImagePath = "bulleit-bourbon-1L.jpeg",
+                        ImagePath = "bulleit-bourbon-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 19.99M
                     };
@@ -70,7 +70,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Fireball Cinnamon Whisky",
-                        ImagePath = "fireball-cinnamon-whisky-1L.jpeg",
+                        ImagePath = "fireball-cinnamon-whisky-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 11.49M
                     };
@@ -81,7 +81,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Jameson Irish Whiskey",
-                        ImagePath = "jameson-irish-whiskey-1L.jpeg",
+                        ImagePath = "jameson-irish-whiskey-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 16.99M
                     };
@@ -92,7 +92,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Maker's Mark Bourbon Whisky",
-                        ImagePath = "makers-mark-bourbon-whisky-1L.jpeg",
+                        ImagePath = "makers-mark-bourbon-whisky-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 19.98M
                     };
@@ -103,7 +103,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Jack Daniel's Old No. 7 Whiskey",
-                        ImagePath = "jack-daniels-old-no-7-1L.jpeg",
+                        ImagePath = "jack-daniels-old-no-7-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 12.99M
                     };
@@ -114,7 +114,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Jim Beam Bourbon Whiskey",
-                        ImagePath = "jim-beam-bourbon-whiskey-1L.jpeg",
+                        ImagePath = "jim-beam-bourbon-whiskey-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 9.99M
                     };
@@ -125,7 +125,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Johnnie Walker Black Label",
-                        ImagePath = "johnnie-walker-black-label-1L.jpeg",
+                        ImagePath = "johnnie-walker-black-label-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 24.99M
                     };
@@ -136,7 +136,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Evan Williams Bourbon Whiskey Black Label",
-                        ImagePath = "evan-williams-black-1L.jpeg",
+                        ImagePath = "evan-williams-black-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 8.79M
                     };
@@ -147,7 +147,7 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Bulleit Rye",
-                        ImagePath = "bulleit-rye-1L.jpeg",
+                        ImagePath = "bulleit-rye-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 19.99M
                     };
@@ -158,11 +158,165 @@ namespace CardinalInventoryWebApi.Data
                         StockItemCategoryId = sic.StockItemCategoryId,
                         StockItemId = Guid.NewGuid(),
                         Description = "Crown Royal Deluxe",
-                        ImagePath = "crown-royal-deluxe-1L.jpeg",
+                        ImagePath = "crown-royal-deluxe-1L.png",
                         UnitSizeMilliliters = 1000,
                         UnitCost = 9.99M
                     };
                     await context.StockItems.AddAsync(whiskey10);
+
+                    var whiskey11 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Buffalo Trace Bourbon",
+                        ImagePath = "buffalo-trace-bourbon-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 9.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey11);
+
+                    var whiskey12 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Knob Creek Kentucky Straight Bourbon Whiskey",
+                        ImagePath = "knob-creek-kentucky-straight-bourbon-whiskey-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 22.80M
+                    };
+                    await context.StockItems.AddAsync(whiskey12);
+
+                    var whiskey13 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Woodford Reserve Bourbon",
+                        ImagePath = "woodford-reserve-bourbon-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 22.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey13);
+
+                    var whiskey14 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Basil Hayden's Kentucky Straight Bourbon Whiskey",
+                        ImagePath = "basil-haydens-kentucky-straight-bourbon-whiskey-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 22.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey14);
+
+                    var whiskey15 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "The Macallan 12 Year Sherry Oak",
+                        ImagePath = "the-macallan-sherry-oak-12-year-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 47.25M
+                    };
+                    await context.StockItems.AddAsync(whiskey15);
+
+                    var whiskey16 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Seagram's 7 Crown Blended Whiskey",
+                        ImagePath = "seagrams-7-crown-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 8.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey16);
+
+                    var whiskey17 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Four Roses Bourbon",
+                        ImagePath = "four-roses-bourbon-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 13.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey17);
+
+                    var whiskey18 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Jack Daniel's Tennessee Honey",
+                        ImagePath = "jack-daniels-tennessee-honey-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 16.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey18);
+
+                    var whiskey19 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Dewar's White Label",
+                        ImagePath = "dewars-white-label-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 17.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey19);
+
+                    var whiskey20 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Hibiki Japanese Harmony Whisky",
+                        ImagePath = "hibiki-japanese-harmony-whisky-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 57.10M
+                    };
+                    await context.StockItems.AddAsync(whiskey20);
+
+                    var whiskey21 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Old Overholt Rye Whiskey",
+                        ImagePath = "old-overholt-original-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 12.49M
+                    };
+                    await context.StockItems.AddAsync(whiskey21);
+
+                    var whiskey22 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Johnnie Walker Red Label",
+                        ImagePath = "johnnie-walker-red-label-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 14.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey22);
+
+                    var whiskey23 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Lagavulin 16 Year",
+                        ImagePath = "lagavulin-16-year-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 61.52M
+                    };
+                    await context.StockItems.AddAsync(whiskey23);
+
+                    var whiskey24 = new StockItem()
+                    {
+                        StockItemCategoryId = sic.StockItemCategoryId,
+                        StockItemId = Guid.NewGuid(),
+                        Description = "Crown Royal Apple",
+                        ImagePath = "crown-royal-regal-apple-1L.png",
+                        UnitSizeMilliliters = 1000,
+                        UnitCost = 18.99M
+                    };
+                    await context.StockItems.AddAsync(whiskey24);
                 }
                 await context.SaveChangesAsync();
             }
